f8219287f05a383821e4125f4b251d1495f4456c
Daniel Strano <stranoj@gmail.com>
2021-12-03 18:27:47 -0500

    Fix requirements

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ import os
 from distutils.core import setup
 
 requirements = [
-    "numpy>=1.13"
+    "numpy>=1.13",
     "pyqrack>=0.7.3"
 ]
 
 # Handle version.
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 # Read long description from README.
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
