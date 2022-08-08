47ff3156ac6ea457b84b5ae7ad6c1c119de9a4e5
Daniel Strano <stranoj@gmail.com>
2022-08-08 08:27:21 -0400

    Use try_separate_1qb() after noise

--- qiskit/providers/qrack/backends/noisy_clifford_t_simulator.py
+++ qiskit/providers/qrack/backends/noisy_clifford_t_simulator.py
@@ -536,6 +536,9 @@ class NoisyCliffordTSimulator(BackendV1):
         self._sim.r(Pauli.PauliZ, -angleZ, qubit)
         self._sim.h(qubit)
 
+        # The original qubit might be below separability threshold.
+        simulator.try_separate_1qb(qubit)
+
     def _apply_op(self, operation, noise):
         name = operation.name
 
