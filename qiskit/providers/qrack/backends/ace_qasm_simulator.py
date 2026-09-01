265937a86d60305056055272b0ce1e6c0e1a3a8a
WrathfulSpatula <stranoj@gmail.com>
2026-09-01 14:06:34 -0400

    Best 16 GB default dimensions

--- qiskit/providers/qrack/backends/ace_qasm_simulator.py
+++ qiskit/providers/qrack/backends/ace_qasm_simulator.py
@@ -69,7 +69,7 @@ class AceQasmSimulator(BackendV2):
     DEFAULT_OPTIONS = {
         # This isn't MPS, but Qiskit won't let us allocate many qubits otherwise.
         'method': 'matrix_product_state',
-        'n_qubits': 64,
+        'n_qubits': 84,
         'shots': 1024,
         'is_schmidt_decompose_multi': False,
         'is_stabilizer_hybrid': False,
@@ -82,8 +82,8 @@ class AceQasmSimulator(BackendV2):
         'is_boundary_repetition_code': False,
         'noise': 0,
         'sdrp': 0.0,
-        'long_range_columns': 4,
-        'long_range_rows': 4,
+        'long_range_columns': 3,
+        'long_range_rows': 7,
         'is_transpose': False,
         'noise_model_infidelty': 0.5,
         'noise_model_damping': 0.5,
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 ]
 
 # Handle version.
-VERSION = "1.12.0"
+VERSION = "1.13.0"
 
 # Read long description from README.
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
