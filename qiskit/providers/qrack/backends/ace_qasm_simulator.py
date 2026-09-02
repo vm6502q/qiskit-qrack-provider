03146e639aa578fccd8be7b2565f2778043b551f
WrathfulSpatula <stranoj@gmail.com>
2026-09-02 12:03:03 -0400

    Count ancillae correctly

--- qiskit/providers/qrack/backends/ace_qasm_simulator.py
+++ qiskit/providers/qrack/backends/ace_qasm_simulator.py
@@ -69,7 +69,7 @@ class AceQasmSimulator(BackendV2):
     DEFAULT_OPTIONS = {
         # This isn't MPS, but Qiskit won't let us allocate many qubits otherwise.
         'method': 'matrix_product_state',
-        'n_qubits': 100,
+        'n_qubits': 96,
         'shots': 1024,
         'is_schmidt_decompose_multi': False,
         'is_stabilizer_hybrid': False,
@@ -82,8 +82,8 @@ class AceQasmSimulator(BackendV2):
         'is_boundary_repetition_code': True,
         'noise': 0,
         'sdrp': 0.0,
-        'long_range_columns': 24,
-        'long_range_rows': 24,
+        'long_range_columns': 23,
+        'long_range_rows': 23,
         'is_transpose': False,
         'noise_model_infidelty': 0.5,
         'noise_model_damping': 0.5,
